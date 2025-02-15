cac84d26129d581bc0106e97e2b3d1127ef3f86d
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2025-02-15 18:59:21 +0200

    feat: added app icon and splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// preventAutoHideAsync();
+
+const SPLASH_SCREEN_DELAY = 2000;
+
 export default function App() {
+  useEffect(() => {
+    // setTimeout(async () => {
+    //   await hideAsync();
+    // }, SPLASH_SCREEN_DELAY);
+  }, []);
+
   return (
     <SafeAreaProvider style={styles.container}>
       <Text>Hello World!</Text>
