494371babb67cbe93edf10d2db01c61891180d15
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2025-02-19 11:07:10 +0200

    test: fixed axios mock

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,13 +1,23 @@
+jest.mock('axios', () => {
+  return {
+    create: jest.fn(() => ({
+      interceptors: {
+        response: {
+          use: jest.fn(),
+        },
+      },
+      get: jest.fn(),
+      post: jest.fn(),
+      patch: jest.fn(),
+      delete: jest.fn(),
+    })),
+  };
+});
+
 jest.mock('react-native-mmkv');
 
 jest.mock('@config/config', () => ({
   config: {
     API_URL: 'http://localhost:8000/api/v1',
-  },
-}));
-
-jest.mock('@store/root-store', () => ({
-  rootStore: {
-    dispatch: jest.fn(),
   },
 }));
